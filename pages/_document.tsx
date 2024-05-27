e236f17a8a0358bbf3ed14372f9735926c35c89f
Bruno Albano <brunoluiz.albano@gmail.com>
2024-05-27 13:04:16 -0300

    add meta tags and favicon default

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
+import Head from 'next/head';
 import Layout from "@/components/layout";
-import type { Metadata } from 'next'
 import Slider from "@/components/slider";
 import Section from "@/components/section";
 import { sliderContent, cmsContent } from '@/content/site-content'
- 
-export const metadata: Metadata = {
-  title: 'Lego',
-  description: 'Lego Description',
-}
+
 
 export default function Page() {
   return (
     <Layout>
+      <Head>
+        <title>LEGO® Sets for Adults | Adults Welcome</title>
+        <meta name="description" content="Here are some of our most complex LEGO® sets designed exclusively for grown-ups. From sport to Star Wars™, vehicles to art, discover LEGO Sets for Adults" />
+      </Head>
       <Slider title={sliderContent.title} video={sliderContent.video} />
       {cmsContent.map((section, index) => (
           <Section key={index} data={section} />
