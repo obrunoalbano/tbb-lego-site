0e002640add55a7a2129653c2a0985f561b1c472
Bruno Albano <brunoluiz.albano@gmail.com>
2024-05-27 12:45:02 -0300

    add break to section content in mobile

--- components/sectionContent.tsx
+++ components/sectionContent.tsx
@@ -14,9 +14,9 @@ interface ContentProps {
 const SectionContent: React.FC<ContentProps> = ( { itemIndex, title, content, image, reverse, cta } ) => {
     return (
         <div className="relative mb-14" >
-            <div className={`flex w-full text-black container mx-auto px-4 ${itemIndex % 2 !== 0 || reverse ? 'section-reverse' : ''}`}>
-                <div className="lg:w-1/2 py-16 flex flex-col justify-center pr-10 section-content">
-                    <div className="max-w-[430px]">
+            <div className={`flex flex-wrap w-full flex-col-reverse text-black container mx-auto px-4 ${itemIndex % 2 !== 0 || reverse ? 'section-reverse' : ''}`}>
+                <div className="lg:w-1/2 py-6 lg:py-16 flex flex-col justify-center pr-4 lg:pr-10 section-content">
+                    <div className="lg:max-w-[430px]">
                         <h2 className="text-3xl mb-6">{title}</h2>
                         <p className="font-light mb-8">{content}</p>
 
@@ -25,7 +25,7 @@ const SectionContent: React.FC<ContentProps> = ( { itemIndex, title, content, im
                         </Button>
                     </div>
                 </div>
-                <div className="lg:w-1/2 lg:absolute inset-y-0 lg:right-0 section-image">
+                <div className="lg:w-1/2 lg:absolute lg:inset-y-0 lg:right-0 section-image">
                     <Image src={image} alt={title} width={940} height={528} className="object-cover w-full h-full"/>
                 </div>
             </div>
